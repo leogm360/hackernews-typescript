92d8dc9ac827c37062f2d924229d44f2da15390a
leogm360 <leogm360@gmail.com>
2022-07-24 13:27:02 -0300

    :bug: hotfix: implement deployment configs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+
 import { context } from "./context";
 import schema from "./schema";
 
 const port = process.env.PORT || 3000;
 
-const server = new ApolloServer({ schema, context });
+const server = new ApolloServer({
+  schema,
+  context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault],
+});
 
 server.listen({ port }).then(({ url }) => {
-  console.log(`🚀 Server is ready at http://localhost:${port}/`);
+  console.log(`🚀 Server is ready at ${url}`);
 });
 
 export default server;
